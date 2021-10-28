94703236ef4dc97a7869bbbf37048fd4d27d9ff0
LucilleBernade <bernadelucille@gmail.com>
2021-10-28 17:14:12 +0200

    lien API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-
+import {React, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import DisplayCode from './components/api/DisplayCode';
+
 
 function App() {
+
+  const getOpenFoodFact = () => {
+    // Send the request 
+    const codebarre = '04963406'
+    axios
+    .get(`https://fr.openfoodfacts.org/api/v2/product/${codebarre}`)
+    .then((response) => response.data)
+    .then((data) => {
+    setProduct(data.product)
+    })
+    }
+
+const [product, setProduct] = useState('')
   return (
     <div className="App">
+      <DisplayCode product={product} />
+      <input type="codebarre" onChange={getOpenFoodFact}></input>
     </div>
   );
 }
