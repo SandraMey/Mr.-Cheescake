6fca241dabebe60ba08c24c3ea34db601206caa0
POHIN Esteban <esteban.pohin86@gmail.com>
2021-10-29 12:27:11 +0200

    Add component alternatives

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import React from 'react';
+import Alternatives from "./components/alternatives/Alternatives";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Alternatives/>
     </div>
   );
 }
