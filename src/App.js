4204d7e574908d642b3e5675dc42ac1051f29533
Paul-Verdure <paul.verdure@gmail.com>
2021-11-04 17:12:03 +0100

    ajout lien maps

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-
-import { React, useState } from "react";
+import { React, useState, useRef, useEffect } from "react";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import axios from "axios";
 import DisplayScan from "./components/Scan/DisplayScan";
-
 import DisplayCode from "./components/api/DisplayCode";
-import Home from "./components/Home";
+import Home from "./components/home/Home";
 
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-
 
 function App() {
   const [product, setProduct] = useState("");
   const getOpenFoodFact = async () => {
     // Send the request
-   await axios
+    await axios
       .get(`https://fr.openfoodfacts.org/api/v2/product/3288131500102`)
       .then((response) => response.data)
       .then((data) => {
@@ -24,46 +21,40 @@ function App() {
       });
   };
 
-  getOpenFoodFact();
-
-
- 
+  useEffect(() => {
+    getOpenFoodFact();
+    
+  }, []);
 
   return (
     <div className="App">
-
       <Header />
       <Router>
-       
-          <div className="nav-btn">
-
-            <button className="buttonmenu">
-              <Link to="/">Home</Link>
-            </button>
-            <button className="buttonmenu">
-              <Link to="/DisplayScan">Je scanne</Link>
-            </button>
-            <button>
-              <Link to="/DisplayCode">Fiche Produit</Link>
-            </button>
-
-          </div>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/DisplayScan">
-              <DisplayScan />
-            </Route>
-            <Route path="/DisplayCode">
-              <DisplayCode product={product} />
-            </Route>
-          </Switch>
-        
+        <div className="nav-btn">
+          <button className="buttonmenu">
+            <Link to="/">Home</Link>
+          </button>
+          <button className="buttonmenu">
+            <Link to="/DisplayScan">Je scanne</Link>
+          </button>
+          <button>
+            <Link to="/DisplayCode">Fiche Produit</Link>
+          </button>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/DisplayScan">
+            <DisplayScan />
+          </Route>
+          <Route path="/DisplayCode">
+            <DisplayCode product={product} />
+          </Route>
+        </Switch>
       </Router>
-      
-      <Footer />
 
+      <Footer />
     </div>
   );
 }
--- src/components/api/DisplayCode.jsx
+++ src/components/api/DisplayCode.jsx
@@ -1,36 +1,17 @@
 import React from 'react';
-import Alternatives from "./AlternativesAlternatives";
-import ProductsInfos from './ProductsInfos';
-import ProductsIngredient from './ProductsIngredient';
-import ProductsNutri from './ProductsNutri';
+import ProductsPack from "./ProductsPack"
 import ProductsProxi from './ProductsProxi';
 import "./DisplayCode.css"
-import ProductsNutri from './ProductsNutri';
 
 
 function DisplayCode({product}) {
-  
   return (
     product && (
       <div>
         <article className='DisplayCode'> 
-          <p className='imageProduit'> Intégrer image </p>
-
-          <div>
-            <ProductsInfos />
-          </div>
-          <div>
-            <ProductsIngredient />
-          </div>
-          <div>
-            <ProductsNutri />
-          </div>
-          <div>
-            <ProductsProxi/>
-          </div>
-          <div>
-            <Alternatives/>
-          </div>
+          
+          <ProductsPack product={product} />
+          <ProductsProxi product={product} />
 
         </article>        
       </div>
